2959d330d0a19b91c3c69a2f24b1e9164ffa4d5e
Rajesh <rajeshshaw.abc@gmail.com>
2023-02-05 13:14:11 +0530

    wire Aex with Parameters

---
+++ force-app/main/default/lwc/wireApexWithParams/wireApexWithParams.js
@@ -0,0 +1,18 @@
+import { LightningElement, wire } from 'lwc';
+import filterAccountType from '@salesforce/apex/filterAccountType.getAccountList';
+
+export default class WireApexWithParams extends LightningElement {
+    selectedType='';
+    @wire(filterAccountType, {type:'$selectedType'})
+    filteredAccounts;
+
+    get typeOptions(){
+        return [
+            {label:"Customer - Channel", value:"Customer - Channel"},
+            {label:"Customer - Direct", value:"Customer - Direct"}
+        ]
+    }
+    typeHandler(event){
+        this.selectedType = event.target.value;
+    }
+}
